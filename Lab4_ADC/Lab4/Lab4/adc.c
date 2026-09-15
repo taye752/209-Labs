8fb8bf06ec942012e3550918c6eeca45901492f1
taye752 <taye752@aucklanduni.ac.nz>
2026-09-15 15:07:51 +1200

    Part 3 code

--- Lab4_ADC/Lab4/Lab4/adc.c
+++ Lab4_ADC/Lab4/Lab4/adc.c
@@ -13,3 +13,10 @@ void adc_init(void) {
 	ADMUX = (1 << REFS0) | (2 << MUX0);
 	ADCSRA = (1 << ADEN) | (1 << ADPS2);
 }
+
+uint16_t adc_read(uint8_t channel) {
+	ADMUX = (ADMUX & 0xF0) | channel;
+	ADCSRA |= (1 << ADSC);
+	while (ADCSRA & (1 << ADSC));
+	return ADC;
+}
--- Lab4_ADC/Lab4/Lab4/adc.h
+++ Lab4_ADC/Lab4/Lab4/adc.h
@@ -11,7 +11,7 @@
 
 #include <stdint.h>
 void adc_init(void);
-uint16_t adc_read(uint8_t chan);
+uint16_t adc_read(uint8_t channel);
 
 
 #endif /* ADC_H_ */
